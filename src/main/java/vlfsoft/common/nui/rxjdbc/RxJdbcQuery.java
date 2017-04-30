bf84fd30043819b504342406c11fd1033f060978
vlfsoft.sup <vlfsoft.sup@gmail.com>
2017-04-30 13:18:13 +0300

    java.time.LocalDateTime support is added.

--- src/main/java/vlfsoft/common/nui/rxjdbc/RxJdbcQuery.java
+++ src/main/java/vlfsoft/common/nui/rxjdbc/RxJdbcQuery.java
@@ -10,7 +10,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Date;
+import java.util.Optional;
 
 import rx.Observable;
 import rx.Subscriber;
@@ -78,11 +81,19 @@ final public class RxJdbcQuery {
 
     }
 
+    /**
+     * See <a href="http://www.java2s.com/Tutorials/Java/Data_Type_How_to/Date_Convert/Convert_java_sql_Timestamp_to_LocalDateTime.htm">Java Data Type How to - Convert java.sql.Timestamp to LocalDateTime</a>
+     * To avoid dependencies doubled this method from vlfsoft.common.nui.rxjdbc.RxJdbcQuery
+     */
+    public static Optional<LocalDateTime> getLocalDateTime(final ResultSet aResultSet, int i) throws SQLException {
+        Timestamp timestamp = aResultSet.getTimestamp(i);
+        return !aResultSet.wasNull() && timestamp != null ? Optional.of(LocalDateTime.ofInstant(timestamp.toInstant(), ZoneOffset.ofHours(0))) : Optional.empty();
+    }
+
     // http://stackoverflow.com/questions/21162753/jdbc-resultset-i-need-a-getdatetime-but-there-is-only-getdate-and-gettimestamp
     public static Date getDateTime(final ResultSet aResultSet, int i) throws SQLException {
         Timestamp timestamp = aResultSet.getTimestamp(i);
-        if (timestamp == null) return null;
-        return new java.util.Date(timestamp.getTime());
+        return !aResultSet.wasNull() && timestamp != null ? new java.util.Date(timestamp.getTime()) : null;
     }
 
 }
